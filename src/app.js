30eb86b5526fdc11d41953f64d2763f34c1afffb
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-02-05 20:17:31 -0300

    se agrego ruta task

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/task.routs.js";
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
-app.use("/api", authRoutes);
 app.use(cookieParser());
+app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Error interno del servidor");
