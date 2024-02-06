f17f4c04afb2d4d69a904ab21d8839bd0ede3149
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-02-05 22:13:48 -0300

    se termina el crud

---
+++ src/models/task.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("task", taskSchema);
--- src/routes/task.routs.js
+++ src/routes/task.routs.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { authRequired } from "../middlelwares/validateToken.js";
+import {
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/task.controller.js";
 
 const router = Router();
 
-router.get("/task", authRequired, (req, res) => res.send("task"));
+router.get("/task", authRequired, getTasks);
+router.get("/task/:id", authRequired, getTask);
+router.post("/tasks", authRequired, createTask);
+router.delete("/task/:id", authRequired, deleteTask);
+router.put("/task/:id", authRequired, updateTask);
 
 export default router;
