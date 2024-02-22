19cf3185e8f629f073a3111abb9d52dc6a10110c
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-02-22 20:10:04 -0300

    se arregla el error de cors

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,4 +1,21 @@
-import axios from "axios";
+/* import axios from "axios";
 const API = "http://localhost:4000/api";
 
 export const registerRequest = (user) => axios.post(`${API}/register`, user);
+ */
+import axios from "axios";
+const API = "http://localhost:4000/api";
+
+export const registerRequest = (user) => {
+  return axios
+    .post(`${API}/register`, user)
+    .then((response) => {
+      // Manejar la respuesta exitosa si es necesario
+      return response.data; // O cualquier otra cosa que necesites devolver
+    })
+    .catch((error) => {
+      // Manejar el error de la solicitud
+      console.error("Error en la solicitud de registro:", error);
+      throw error; // Puedes lanzar el error nuevamente para que el código que llamó a esta función pueda manejarlo
+    });
+};
--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
-import taskRoutes from "./routes/task.routs.js";
+import taskRoutes from "./routes/task.routes.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
